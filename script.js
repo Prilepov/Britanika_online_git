ec80b07319f79c4bebc2d79b98bd21b7cbb4c519
DiLess <zzloba@gmail.com>
2022-09-06 16:09:05 +0300

    Done Photo & Video slider

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Slick carousel */
+
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    infinite: true,
+    prevArrow: "<img src='images/prev_arrow.png' class='prevar' alt='1'>",
+    nextArrow: "<img src='images/nex_arrow.png' class='nextar' alt='1'>"
+});
