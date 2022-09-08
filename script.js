2aeb8134eff65072c540db96a3332eccfa18c121
DiLess <zzloba@gmail.com>
2022-09-08 17:20:25 +0300

    Done car series

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 /* Slick carousel */
-
 $('.center').slick({
     centerMode: true,
-    centerPadding: '60px',
+    // centerPadding: '60px',
     slidesToShow: 5,
     infinite: true,
     prevArrow: "<img src='images/prev_arrow.png' class='prevar' alt='1'>",
     nextArrow: "<img src='images/nex_arrow.png' class='nextar' alt='1'>"
 });
+
+/* Series slider */
+$('.series_slider').slick({
+    // centerMode: true,
+    // centerPadding: '60px',
+    // slidesToShow: 3,
+    dots: true,
+    arrows: false,
+    infinite: true,
+})
