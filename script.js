6805b906c4b7a7a12bd32b25cba836644531840d
DiLess <zzloba@gmail.com>
2022-09-16 16:21:11 +0300

    Popup window without responsive

--- script.js
+++ script.js
@@ -17,3 +17,46 @@ $('.series_slider').slick({
     arrows: false,
     infinite: true,
 })
+
+/* Popup slider */
+let slick = $('.popup_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    prevArrow: "<img src='images/prev_arrow.png' class='prevar_popup' alt='1'>",
+    nextArrow: "<img src='images/nex_arrow.png' class='nextar_popup' alt='1'>"
+})
+
+/* Pop-up */
+$(document).ready(function($) {
+	$('.popup-open').click(function() {
+		$('.popup-fade').fadeIn();
+		return false;
+	});	
+	
+	$('.popup-close').click(function() {
+		$(this).parents('.popup-fade').fadeOut();
+		return false;
+	});		
+ 
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade').fadeOut();
+		}
+	});
+	
+	$('.popup-fade').click(function(e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();					
+		}
+	});
+});
+
+$('.popup-open').click(function() {
+    slick.slick('refresh');
+});
