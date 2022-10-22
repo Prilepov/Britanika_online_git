a70c7b8d8978fceea486ac0eca1ce71473e26713
DiLess <zzloba@gmail.com>
2022-10-22 16:31:36 +0300

    Done Select switcher

--- script.js
+++ script.js
@@ -67,3 +67,13 @@ $( document ).ready(function() {
       }
     })
   })
+
+/* Select series switcher */
+$( document ).ready(function() {
+  $(".switcher_select").click(function(e) {
+    $(this).addClass('active');{
+      $('.tab-select.active').removeClass('active');
+      $('.tab-select.' + $(this).data('target')).addClass('active');
+    }
+  })
+})
